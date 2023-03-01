a9775316b36c6c69f7ba4198d75d6cfe291627b3
javijanices <javierjanices@gmail.com>
2023-02-28 23:55:27 -0300

    nuevo avance

--- funciones.js
+++ funciones.js
@@ -2,3 +2,5 @@ function crearElemento (tipo){
     return document.createElement(tipo);
 }
 
+let longString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam placerat elit sit amet nibh varius dictum. Proin volutpat enim eget neque suscipit, in ornare nisl luctus. Nullam aliquet est non arcu consequat sollicitudin.";    
+ 
