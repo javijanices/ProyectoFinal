a6def8fd53ac3e8ebe4021d90f5db118bb3da20d
javijanices <javierjanices@gmail.com>
2023-03-07 23:34:00 -0300

    nuevo avance

--- funciones.js
+++ funciones.js
@@ -1,6 +1,31 @@
-function crearElemento (tipo){
-    return document.createElement(tipo);
+// menu Lateral
+
+var menuVisible = false;
+let menu = document.getElementById("nav");
+function mostrarMenu(){
+    if(menuVisible == false){
+        menu.style.display = "block";
+        menuVisible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menuVisible = false;
+    }
 }
 
-let longString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam placerat elit sit amet nibh varius dictum. Proin volutpat enim eget neque suscipit, in ornare nisl luctus. Nullam aliquet est non arcu consequat sollicitudin.";    
- 
+let vinculos = document.querySelectorAll("nav a");
+for(var i=0; i < vinculos.length; i++){
+    vinculos[i].onclick = function(){
+        menu.style.display = "none";
+        menuVisible = false;
+    }
+}
+
+function lorem1(){
+    return Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem neque repellendus ad incidunt doloremque sapient;
+}
+
+function lorem2(){
+    return Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam placerat elit sit amet nibh varius dictum;
+}
+
